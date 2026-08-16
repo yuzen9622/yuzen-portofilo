745e456f9ee1f74edb1bbcf5a30641ff3960f36d
yuzen9622 <oscar48079@gmail.com>
2026-08-16 21:47:13 +0800

    refactor(home): remove unused services section

--- feature/home/section/services.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { SectionLayout } from "../components/section-layout";
-
-export default function Services() {
-  return <SectionLayout id="service"></SectionLayout>;
-}
